8715445844f32576e73605c5bd836c7fcc0d8193
volodyatrots <vova_lvova@i.ua>
2018-01-28 15:01:26 +0200

    add findSum

--- script.js
+++ script.js
@@ -5,22 +5,24 @@ var	spanPrice=$(this).prev()[0];
 var	price=spanPrice.innerHTML;
 var devices=$(spanPrice).parent().parent();
 var name=devices[0].getElementsByClassName("name")[0].innerHTML;
-
 	if(this.count===undefined || this.count==0){
 		this.count=1;
 	}else{
 	this.count++;
 	addCount(this.count,name)
+	findSum()
 	return;
 }	
 
 var elem=$("<tr> <td>"+name+"<td>"+price+"<td>"+this.count+"<td>");
 $(elem).append("<td><button class='rem'>X</button><td>");
 $(".cartTable").append(elem);
+findSum();
 
 $(".rem").on("click",function(){
-    button.count=0;
+	button.count=0;
 	$(this).parent().parent().remove();
+	findSum()
 });	
 });
 
@@ -30,8 +32,20 @@ $(".confirmButton").on("click",function(){
 
 $(".clearButton").click(function(){
 	$(".rem").click();	
+	$(".totalSum").empty();
 });	
 });
+
+function findSum(){
+	var sum=0;
+	var prices=$("td:nth-of-type(2)");
+	var counts=$("td:nth-of-type(3)");
+	for(var i=0;i<prices.length;i++){
+		sum+=parseInt((prices[i].innerHTML))*counts[i].innerHTML;
+	}
+	$(".totalSum").text("Total: "+sum)
+}
+
 
  function addCount(countDevice,name){
 	 var namesDevices=$("td:first-child");
